9b23966009287465648596ca00bb9d0512684d2a
turboKnul <ncdeschipper@gmail.com>
2017-03-13 20:40:15 +0100

    Full color support! Fly your ship in style!

--- rocketship.sh
+++ rocketship.sh
@@ -3,14 +3,18 @@
 ############################################
 # Free rocketship simulator 
 # Bringing you free rocketships since 2017
-############################################
+###########################################
 
 #engage dangerous chris mode
-if  [[ $1 = "-chris" ]]; then
+if  [[ $* == *-chris* ]]; then
  
 	printf "(_)_):::::::::::D " 
-
+	
 	while :; do
+		
+		if [[ $* == *-rainbow* ]]; then
+			tput setaf $(( 1 + $RANDOM % 6 )) 
+		fi
 		printf "~"
 		sleep 0.03s
 	done
@@ -23,6 +27,9 @@ else
 		
 		rand=$(( ( RANDOM % 10 )  + 1 ))
 		if [ $rand -gt 3 ]; then 
+			if [[ $* == *-rainbow* ]]; then
+				tput setaf $(( 1 + $RANDOM % 6 )) 
+			fi
 			printf "~"
 		else
 			printf " "
@@ -32,6 +39,10 @@ else
 	done
 	printf "\n"
 fi
+
+
+
+
 
 
  
